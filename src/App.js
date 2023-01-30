2f6d0390e5bf8c2793097fd15c8f033a4db85759
Pamelar10 <116229630+Pamelar10@users.noreply.github.com>
2023-01-29 21:27:32 -0500

    added the github repo url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Weather App </div>;
+  return <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      This project was coded by Pamela Rosales and is 
+  <a href="https://github.com/Pamelar10/react-weather-app" 
+  target="_blank"> open-sourced on GitHub</a>
+      </footer>
+   </div>;
 }
 
 export default App;
