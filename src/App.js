55d3c6d7483ae79c3937fe4c0aa872daa08131ec
Pamelar10 <116229630+Pamelar10@users.noreply.github.com>
2023-01-30 20:09:05 -0500

    added divs and more info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-
+import React from "react";
 import './App.css';
+import Weather from "./Weather";
+
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Pamela Rosales and is
           <a
             href="https://github.com/Pamelar10/react-weather-app"
-            target="_blank"
+            
           >
             {" "}
             open-sourced on GitHub
