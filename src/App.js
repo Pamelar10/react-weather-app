08183891a86a2cdd24a81667d20a99f810e01d5a
Pamelar10 <116229630+Pamelar10@users.noreply.github.com>
2023-01-30 19:36:17 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@
 import './App.css';
 
 function App() {
-  return <div className="App">
-    <h1>Weather App</h1>
-    <footer>
-      This project was coded by Pamela Rosales and is 
-  <a href="https://github.com/Pamelar10/react-weather-app" 
-  target="_blank"> open-sourced on GitHub</a>
-      </footer>
-   </div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Pamela Rosales and is
+          <a
+            href="https://github.com/Pamelar10/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
