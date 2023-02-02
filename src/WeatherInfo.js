1e03a6db20ed1f18843fec8681d6307231530be5
Pamelar10 <116229630+Pamelar10@users.noreply.github.com>
2023-02-02 17:00:05 -0500

    added weather icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,39 +1,38 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon"
 
 export default function Weatherinfo (props) {
-    return ( 
-        <div className="Weather Info">
-            <h1>{props.data.city}</h1>
+    return (
+      <div className="Weather Info">
+        <h1>{props.data.city}</h1>
         <ul>
           <li>
             <FormattedDate date={props.data.date} />
           </li>
-          <li className="text-capitalize">
-          {props.data.description}</li>
+          <li className="text-capitalize">{props.data.description}</li>
         </ul>
         <div className="row mt-3">
           <div className="col-6">
             <div className="clearfix">
-              <img
-                src={props.data.iconUrl}
-                alt={props.data.description}
-                className="float-left"
-              />
               <div className="float-left">
-                <span className="temperature">{Math.round (props.data.temperature)}</span>
+                <WeatherIcon code={props.data.icon} />
+              </div>
+              <div className="float-left">
+                <span className="temperature">
+                  {Math.round(props.data.temperature)}
+                </span>
                 <span className="unit">°F</span>
               </div>
             </div>
           </div>
           <div className="col-6">
             <ul>
-              
               <li>Humidity: {props.data.humidity}%</li>
               <li>Wind: {props.data.wind} mph</li>
             </ul>
           </div>
         </div>
-        </div>
+      </div>
     );
 }
