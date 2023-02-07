475ae5cd76324134085282e7bb1992c45ea16250
Pamelar10 <116229630+Pamelar10@users.noreply.github.com>
2023-02-07 13:22:17 -0500

    updating forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,13 +20,15 @@ export default function WeatherForecast(props) {
          <div className="row">
            <div className="col">
              <div className="WeatherForecast-day">
-               Thu
-               <WeatherIcon code="01d" size={36} />
+               {forecast[0].dt}
+               <WeatherIcon code={forecast[0].weather[0].icon} size={36} />
                <div className="WeatherForecast-temperatures">
                  <span className="WeatherForecast-temperature-max">
                    {forecast[0].temp.max}°
                  </span>
-                 <span className="WeatherForecast-temperature-min">{forecast[0].temp.min}°</span>
+                 <span className="WeatherForecast-temperature-min">
+                   {forecast[0].temp.min}°
+                 </span>
                </div>
              </div>
            </div>
