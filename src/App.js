388ab82e98d30e83e6e450b279918833f46061ad
Pamelar10 <116229630+Pamelar10@users.noreply.github.com>
2023-02-09 14:50:32 -0500

    added effect and cleaned up code

--- src/App.js
+++ src/App.js
@@ -8,12 +8,25 @@ function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="New York" />
-       
+
         <footer>
           This project was coded by Pamela Rosales and is
-          <a href="https://github.com/Pamelar10/react-weather-app">
-            {" "}
+          <a
+            href="https://github.com/Pamelar10/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {""}
             open-sourced on GitHub
+          </a>{" "}
+          {""}
+          and is {""}
+          <a
+            href="https://snazzy-daffodil-0cbb13.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
